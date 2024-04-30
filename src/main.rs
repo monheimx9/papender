25fb3c7b4cbe30e8d77bbb64c68aacc78efccafc
Jeremy D <59767686+monheimx9@users.noreply.github.com>
2024-04-30 10:54:11 +0200

    Update main.rs
    Anyhow and error management

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod args;
 mod imgproc;
+use anyhow::Result;
 
 /// Example showcasing a generic implementation of image concatenation.
 ///
@@ -8,7 +9,7 @@ mod imgproc;
 /// Run from the root of the repository with:
 /// cargo run --release --example concat
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct LesFiltres {
     pub hue: Option<i32>,
     pub contrasty: Option<f32>,
@@ -16,25 +17,30 @@ pub struct LesFiltres {
     pub invert: bool,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = args::config_load()?;
-    let new_y: u32 = opts.resize.unwrap_or(400);
-    let imgs = match &opts.flagos {
-        Some(flags) => flags.iter().map(image::open).collect(),
-        None => {
-            println!("No images to concat");
-            Ok(vec![])
+fn main() -> Result<()> {
+    let opts = &args::config_load().unwrap().clone();
+    let mut imgs = Vec::new();
+    let new_y: &u32 = &opts.resize.unwrap();
+    match &opts.flagos {
+        Some(flags) => {
+            for img in flags {
+                imgs.push(image::open(img)?);
+            }
         }
-    }?;
+        None => println!("No images to concat"),
+    }
     let image_last = {
         if opts.no_resize {
-            image::open(&opts.input)?
+            image::open(opts.input.clone().unwrap())?
         } else {
-            let rescaled = imgproc::scale_image(&opts.input, new_y);
+            let rescaled = imgproc::scale_image(opts.input.clone().unwrap(), new_y)?;
             image::open(rescaled)?
         }
     };
-    let result_path = &opts.output;
+    let result_path = &opts.output.clone().unwrap();
 
-    Ok(imgproc::h_concat_vec(imgs, image_last, opts.les_filtres).save(result_path)?)
+    imgproc::h_concat_vec(imgs, image_last, opts.les_filtres.clone())
+        .save(result_path)
+        .unwrap();
+    Ok(())
 }
